0034654cd65347d7edeb314e38a2d5c33e42e3e6
anna <anna@developmentontheedge.com>
2025-03-25 12:39:50 +0700

    TextUtil: add some methods

--- src/main/java/ru/biosoft/util/TextUtil.java
+++ src/main/java/ru/biosoft/util/TextUtil.java
@@ -1,9 +1,14 @@
 package ru.biosoft.util;
 
+import java.text.DecimalFormat;
 import java.util.Locale;
 
 /**
  * Various utils for working with text date.
+ */
+
+/**
+ * Print size in human readable format like '8.2Gb (8,818,826,886 bytes)'
  */
 public class TextUtil
 {
@@ -16,6 +21,37 @@ public class TextUtil
         return i == 0
                 ? size == 1 ? "1 byte" : String.format(Locale.ENGLISH, "%,d bytes", size)
                 : String.format(Locale.ENGLISH, "%.1f%cb (%,d bytes)", normalizedSize, suffixes.charAt(i - 1), size);
+    }
+
+    /**
+     * Returns string, that represents double value, with specified number of
+     * decimal digits.
+     *
+     * If the value indeed is the integer, than the decimal digits are omitted.
+     *
+     * @param value double value
+     * @param decDig number of decimal digits
+     */
+    public static String valueOf(double value, int decDig)
+    {
+        StringBuilder decs = new StringBuilder("");
+        if( decDig > 0 )
+        {
+            decs.append('.');
+            for ( int i = 0; i < decDig; i++ )
+                decs.append('#');
+        }
+        return new DecimalFormat("##,###,###,###,###,###,###,###,###,###,###" + decs).format(value);
+    }
+
+    /**
+     * Returns string with first letter capitalized
+     */
+    public static String ucFirst(String string)
+    {
+        if( string == null )
+            return null;
+        return Character.toTitleCase(string.charAt(0)) + string.substring(1);
     }
 
 }
